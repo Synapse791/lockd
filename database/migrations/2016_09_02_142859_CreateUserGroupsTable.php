f8d05b5d909a88d1a4b45fa4b34cc526bd2e99cf
Synapse791 <synapse791@gmail.com>
2016-09-02 15:42:29 +0100

    Adding user groups table

---
+++ database/migrations/2016_09_02_142859_CreateUserGroupsTable.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('au_user_groups', function (Blueprint $table) {
+            $table->integer('user_id');
+            $table->integer('group_id');
+
+            $table->primary(['user_id', 'group_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('au_user_groups');
+    }
+}
