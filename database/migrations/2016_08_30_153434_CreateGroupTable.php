b2bbee27e09a97e21dcd143b88f5db2aa3aa0dd0
Synapse791 <synapse791@gmail.com>
2016-09-02 14:56:16 +0100

    Adding unique flag to Group names

--- database/migrations/2016_08_30_153434_CreateGroupTable.php
+++ database/migrations/2016_08_30_153434_CreateGroupTable.php
@@ -14,7 +14,7 @@ class CreateGroupTable extends Migration
     {
         Schema::create('au_group', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->unique();
             $table->timestamps();
         });
     }
