8c8de0538d8293607e7a7ca336340f41a429e772
Synapse791 <synapse791@gmail.com>
2016-09-02 10:20:04 +0100

    Added UserRepository contract

---
+++ app/Contracts/Repositories/UserRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Lockd\Contracts\Repositories;
+
+/**
+ * Interface UserRepository
+ *
+ * @package Lockd\Contracts\Repositories
+ * @author Iain Earl <synapse791@gmail.com>
+ */
+interface UserRepository
+{
+    /**
+     * Return users that match the specified parameters
+     *
+     * @param array $parameters
+     * @return \Illuminate\Database\Eloquent\Collection|null
+     */
+    public function find(array $parameters);
+
+    /**
+     * Find all users
+     *
+     * @return \Illuminate\Database\Eloquent\Collection|null
+     */
+    public function findAll();
+
+    /**
+     * Find a single user by their ID
+     *
+     * @param $id
+     * @return \Lockd\Models\User|null
+     */
+    public function findOneById($id);
+
+    /**
+     * Find a single user by their email
+     *
+     * @param $email
+     * @return \Lockd\Models\User|null
+     */
+    public function findOneByEmail($email);
+
+    /**
+     * Counts the users in the database
+     *
+     * @return int
+     */
+    public function count();
+}
